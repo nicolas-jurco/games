999672044f9e5b28a216b4bb7a3b4bb5b4b1e6aa
aknicolas <anicolas.kis@gmail.com>
2021-04-04 15:05:37 -0300

    Added snake head

--- snake/snake.js
+++ snake/snake.js
@@ -2,6 +2,9 @@
 // TODO: change the size of the .grid dinamically depending on these consts
 const COLS = 10
 const ROWS = 10
+const SPEED = 0.2
+let intervalTime = 1000
+let interval = 0
 
 let Direction = {
     'Up': -COLS,
@@ -12,8 +15,7 @@ let Direction = {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    createDivs()
-    initGame()
+    startGame()
 });
 
 // Save all existing elements on const
@@ -26,30 +28,28 @@ let snakePosition = [0,1,2] // 0 index is head, last index is tail
 let direction = Direction.Init
 let divs = []
 
-// TODO: Create all the divs dinamically
-function createDivs(){
+// Start game configuration
+function startGame(params) {
     for (let index = 0; index < COLS * ROWS; index++) {
         let element = document.createElement("div")
         element.setAttribute("id", index)
         document.getElementsByClassName("grid")[0].appendChild(element)
     }
-}
-
-// Start game configuration
-function initGame(params) {
     snakePosition[0] = getRandomIntInclusive(0, COLS * ROWS)
     snakePosition[1] = snakePosition[0] + 1
     snakePosition[2] = snakePosition[1] + 1
 
-    applePosition = getRandomIntInclusive(0, COLS * ROWS)
-    
+    newApple()
+
     divs = document.querySelectorAll(".grid div")
-
-    document.getElementById(applePosition).classList.add('apple')
-
+    
     snakePosition.forEach(element => {
         divs[element].classList.add('snake')
     });
+
+    intervalTime = intervalTime * SPEED
+    interval = setInterval(moveSnake, intervalTime)
+
 }
 
 // TODO: Snake movement logic
@@ -58,19 +58,28 @@ function initGame(params) {
 // for each movement, must check if overflow
 function moveSnake(){
     let newSnakePosition = snakePosition[0] + direction
+    if(direction === Direction.Init)
+        return
     // TODO: check if snake hits himself
     if(snakePosition.find((element) => element === newSnakePosition)){
-        // ? lose the game
+        alert('you lost')
+        // TODO: restart the game
         return
     }
     // TODO: check if snake hits with borders
     
     // TODO: check if snake hits apple
-
-    divs[snakePosition[snakePosition.length-1]].classList.remove('snake')
-    snakePosition.pop()
+    if(newSnakePosition !== applePosition){
+        divs[snakePosition[snakePosition.length-1]].classList.remove('snake')
+        snakePosition.pop()
+    }else{
+        document.getElementById(applePosition).classList.remove('apple')
+        newApple()
+    }
+    divs[snakePosition[0]].classList.remove('snakehead') // remove the head class
+    divs[snakePosition[0]].classList.add('snake') 
     snakePosition.unshift(newSnakePosition)
-    divs[snakePosition[0]].classList.add('snake')
+    divs[snakePosition[0]].classList.add('snakehead')
 }
 
 // TODO: Handle key events
@@ -87,8 +96,18 @@ document.addEventListener('keyup', (e) => {
         direction = Direction.Down
         console.log(direction);
     }
-    moveSnake()
+    // moveSnake()
 })
+
+function newApple() {
+    applePosition = getRandomIntInclusive(0, COLS * ROWS)
+    let element = document.getElementById(applePosition)
+    if(element.classList.contains('snake') || element.classList.contains('snakehead')){
+        newApple()
+        return
+    }
+    element.classList.add('apple')
+}
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
